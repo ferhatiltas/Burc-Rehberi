acc09fbc53d34a2a6d14001a7db9953749149599
Ferhat İltaş <iltasferhatt@gmail.com>
2021-01-08 00:38:09 +0300

    Add files via upload

---
+++ lib/ui/burc_detay.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BurcDetay extends StatelessWidget {
+  int gelenIndex;
+
+  BurcDetay(this.gelenIndex);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container( );
+  }
+}
