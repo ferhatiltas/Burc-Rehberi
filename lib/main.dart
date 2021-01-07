8a3717af121f64ee3702b5c684b2d51fd5ef09fc
Ferhat İltaş <iltasferhatt@gmail.com>
2021-01-07 22:11:13 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'package:burc_rehberi/ui/burc_listesi.dart';
 import 'package:burc_rehberi/ui/burc_rehberi_demo.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      title: "Burç Rehberi",
+void main() =>
+    runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.blue
+          primarySwatch: Colors.purple
       ),
-      home: BurcRehberiDemo(),
-    ),
-  );
+      title: "Burç Rehperi",
+      home: BurcListesi(),
+
+    );
+  }
 }
